c85de457703b8dc4a9d8d7faa1fc11ddbea1e19d
Tim Holy <tim.holy@gmail.com>
2016-11-12 10:10:54 -0600

    Initial working commit

--- src/OptimTests.jl
+++ src/OptimTests.jl
@@ -1,5 +1,80 @@
 module OptimTests
 
-# package code goes here
+using Optim, CUTEst
+
+export solve_problem, solution_optimum
+
+function symmetrize!(h)
+    for j = 1:size(h,2)
+        for i = 1:j-1
+            h[i,j] = h[j,i]
+        end
+    end
+    h
+end
+
+function cutest_fg!(nlp, x, g)
+    fval,gval = objgrad(nlp, x, true)
+    copy!(g, gval)
+    fval
+end
+
+function cutest_hess!(nlp, x, h)
+    symmetrize!(copy!(h, hess(nlp, x)))
+end
+
+function cutest_jacobian!(nlp, x, J)
+    j = jac(nlp, x)
+    copy!(J, j)
+end
+
+function cutest_constr_hess!(nlp, x, λ, h)
+    hx = symmetrize!(hess(nlp, x; obj_weight=0.0, y=λ))
+    h[:,:] += hx
+    h
+end
+
+function solve_problem(nlp::CUTEstModel, options)
+    x0 = nlp.meta.x0
+    d = TwiceDifferentiableFunction(x->obj(nlp, x),
+                                    (x,g)->copy!(g, grad(nlp, x)),
+                                    (x,g)->cutest_fg!(nlp, x, g),
+                                    (x,h)->cutest_hess!(nlp, x, h))
+    if nlp.meta.ncon == 0 && isempty(nlp.meta.ilow) && isempty(nlp.meta.iupp)
+        result = optimize(d, TwiceDifferentiableConstraintsFunction(Float64[],Float64[]),
+                          x0, IPNewton(), options)
+    else
+        constraints = TwiceDifferentiableConstraintsFunction(
+            (x,c)->cons!(nlp, x, c),
+            (x,J)->cutest_jacobian!(nlp, x, J),
+            (x,λ,h)->cutest_constr_hess!(nlp, x, λ, h),
+            nlp.meta.lvar, nlp.meta.uvar, nlp.meta.lcon, nlp.meta.ucon)
+        if !isinterior(constraints, x0)
+            return Inf
+        end
+        result = optimize(d, constraints, x0, Optim.IPNewton(), options)
+    end
+    result
+end
+
+function solve_problem(prob::AbstractString, options)
+    nlp = CUTEstModel(prob)
+    local result
+    try
+        result = solve_problem(nlp, options)
+    finally
+        finalize(nlp)
+    end
+    result
+end
+
+function solution_optimum(prob::AbstractString)
+    str = readstring(joinpath(ENV["MASTSIF"], addsif(prob)))
+    m = match(r"SOLTN +([0-9\.\-D]+)", str)
+    m == nothing && return NaN
+    parse(Float64, replace(m.captures[1], "D", "e"))
+end
+
+addsif(prob::AbstractString) = endswith(prob, ".SIF") ? prob : string(prob, ".SIF")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
-using OptimTests
+using OptimTests, Optim
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+options = OptimizationOptions()
+problems = CUTEst.select(max_var=100,max_con=100,custom_filter=x->x["derivative_order"]>=2)
+results = Dict{String,Any}()
+for prob in problems
+    local result
+    @show prob
+    try
+        result = solve_problem(prob, options)
+    catch
+        results[prob] = "failed"
+        continue
+    end
+    if result == Inf
+        println("Initial point is not in the interior")
+        continue
+    end
+    results[prob] = (Optim.converged(result), result.minimum, solution_optimum(prob), result.iterations, result.f_calls)
+end
