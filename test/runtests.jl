8c69988c392bb9c8d40eb17ebc500f77187ebcd7
Tim Holy <tim.holy@gmail.com>
2016-11-19 03:21:10 -0600

    Collect more data, including Ipopt convergence statistics

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,45 @@
-using OptimTests, Optim
+using OptimTests, Optim, CUTEst, NLPModels, MathProgBase, Ipopt
 using Base.Test
 
+ipoptdata = Float64[]
+function intermediate(alg_mod::Int, iter_count::Int,
+                      obj_value::Float64, inf_pr::Float64, inf_du::Float64, mu::Float64,
+                      d_norm::Float64, regularization_size::Float64, alpha_du::Float64,
+                      alpha_pr::Float64, ls_trials::Int)
+    push!(ipoptdata, obj_value)
+    return true
+end
+
+immutable Results
+    converged::Bool
+    obj_optim::Float64
+    obj_ipopt::Float64
+    obj_cutest::Float64
+    cv_initial::Float64
+    cv_optim::Float64
+    cv_ipopt::Float64
+    iter_optim::Int
+    iter_ipopt::Int
+    fcalls::Int
+    objtrace_optim::Vector{Float64}
+    objtrace_ipopt::Vector{Float64}
+end
+
 # unconstrained
+options = OptimizationOptions(store_trace=true)
 unc_problems = CUTEst.select(max_var=10,contype = :unc, custom_filter=x->x["derivative_order"]>=2)
 unc_results = Dict{String,Any}()
 for prob in unc_problems
     local result
     @show prob
+    nlp = CUTEstModel(prob)
+    d, constraints = optim_problem(nlp)
+    x0 = initial_x(nlp)
     try
-        result = solve_problem(prob, GradientDescent())
+        result = solve_problem(d, constraints, x0, GradientDescent(), options)
     catch
         unc_results[prob] = "failed"
+        finalize(nlp)
         continue
     end
     if result == Inf
@@ -18,23 +47,45 @@ for prob in unc_problems
         continue
     end
     unc_results[prob] = (Optim.converged(result), result.minimum, solution_optimum(prob), result.iterations, result.f_calls)
+    finalize(nlp)
 end
 
 # constrained
-con_problems = CUTEst.select(max_var=100,max_con=100,custom_filter=x->x["derivative_order"]>=2)
-con_results = Dict{String,Any}()
-for prob in con_problems
+options = OptimizationOptions(store_trace=true)
+problems = CUTEst.select(max_var=100,max_con=100,custom_filter=x->x["derivative_order"]>=2)
+results = Dict{String,Any}()
+for prob in problems
     local result
     @show prob
-    try
-        result = solve_problem(prob, IPNewton())
-    catch
-        con_results[prob] = "failed"
-        continue
-    end
-    if result == Inf
+    nlp = CUTEstModel(prob)
+    d, constraints = optim_problem(nlp)
+    x0 = initial_x(nlp)
+    if !isinterior(constraints, x0)
         println("Initial point is not in the interior")
+        finalize(nlp)
         continue
     end
-    con_results[prob] = (Optim.converged(result), result.minimum, solution_optimum(prob), result.iterations, result.f_calls)
+    try
+        result = solve_problem(d, constraints, x0, IPNewton(), options)
+    catch
+        results[prob] = "failed"
+        finalize(nlp)
+        continue
+    end
+    # Compare against Ipopt
+    model = NLPtoMPB(nlp, IpoptSolver())
+    setIntermediateCallback(model.inner, intermediate)
+    empty!(ipoptdata)
+    MathProgBase.optimize!(model)
+    # Collect statistics
+    niter = min(result.iterations, length(ipoptdata))
+    objOptim = [tr.value for tr in result.trace[1:niter]]
+    objIpopt = ipoptdata[1:niter]
+    results[prob] = Results(Optim.converged(result), result.minimum,
+                            MathProgBase.getobjval(model), solution_optimum(prob),
+                            eqconstraints_violation(nlp, nlp.meta.x0),
+                            eqconstraints_violation(nlp, result.minimizer),
+                            eqconstraints_violation(nlp, MathProgBase.getsolution(model)),
+                            result.iterations, length(ipoptdata), result.f_calls, objOptim, objIpopt)
+    finalize(nlp)
 end
